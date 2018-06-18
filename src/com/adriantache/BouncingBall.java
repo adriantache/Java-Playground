745e89b2639306e626e48be6fe15b74cee786cc5
Adrian Tache <contact@adriantache.com>
2018-06-18 09:24:55 +0300

    Add main menu

---
+++ src/com/adriantache/BouncingBall.java
@@ -0,0 +1,8 @@
+package com.adriantache;
+
+public class BouncingBall {
+
+    public static void main(){
+        System.out.println("It works!");
+    }
+}
--- src/com/adriantache/Main.java
+++ src/com/adriantache/Main.java
@@ -1,8 +1,33 @@
 package com.adriantache;
 
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        char input = '!';
+
+        System.out.println("Choose an option:\n1. Bouncing Ball\n");
+
+        //get user input
+        try {
+            input = (char) System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //exhaust buffer
+        char buffer = '!';
+        while (buffer!='\n') {
+            try {
+                buffer = (char) System.in.read();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //process input
+        if (input == '1') BouncingBall.main();
+        else  System.out.println("Illegal option!");
     }
 }
