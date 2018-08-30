e765b07b65aef4334d08ecf3123d5afddae0f9ff
Adrian Tache <contact@adriantache.com>
2018-08-30 15:27:38 +0300

    Add printf

--- src/com/adriantache/stream_practice/StreamPractice.java
+++ src/com/adriantache/stream_practice/StreamPractice.java
@@ -17,7 +17,7 @@ public class StreamPractice {
 
         System.out.println("Choose an option:\n" +
                 "1. Read an array of bytes\n" +
-                "2. \n" +
+                "2. Print format\n" +
                 "3. \n" +
                 "4. \n" +
                 "5. \n" +
@@ -51,7 +51,7 @@ public class StreamPractice {
                 readArrayOfBytes();
                 break;
             case '2':
-
+printf();
                 break;
             case '3':
 
@@ -75,11 +75,14 @@ public class StreamPractice {
     }
 
     private static void readArrayOfBytes() {
-        byte[] data = new byte[10];
         System.out.println("Enter some characters. (10 max)");
 
         try {
-            System.in.read(data);
+            byte[] data = new byte[10];
+            int bytesAvailable = System.in.available();
+            int bytesRead = System.in.read(data);
+
+            System.out.println(bytesRead + " bytes read out of " + bytesAvailable + " bytes available.");
 
             System.out.print("You entered: ");
             for (int i = 0; i < data.length; i++) {
@@ -87,12 +90,20 @@ public class StreamPractice {
             }
 
             //exhaust buffer if input was >10 chars
-            char buffer = '!';
+            char buffer = (char) System.in.read();
             while (buffer != '\n')
                 buffer = (char) System.in.read();
         } catch (IOException e) {
             System.out.println("Error.");
         }
+    }
+
+    private static void printf() {
+        System.out.println("Printing characters using a format.");
+
+       System.out.printf("%4$2s %3$2s %2$2s %1$2s", "a", "b", "c", "d");
+       System.out.println();
+       System.out.format("%4$2s %3$2s %2$2s %1$2s", "a", "b", "c", "d");
     }
 
 }
